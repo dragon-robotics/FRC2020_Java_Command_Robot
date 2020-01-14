ac33f61c5335365e7276ac2f3ae903acd89ae41d
Dragon Robotics <dragonrobotics2375@gmail.com>
2020-01-14 16:30:36 -0700

    1. Fixed the Climb_Up and Climb_Down command

--- src/main/java/frc/robot/commands/Climb_Down.java
+++ src/main/java/frc/robot/commands/Climb_Down.java
@@ -38,12 +38,12 @@ public class Climb_Down extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-
+      
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return true;
+        return false;
     }
 }
