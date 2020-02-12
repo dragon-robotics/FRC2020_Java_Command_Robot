8fd407f97830829c15ba40111984c63729b80b53
Doug Liu <dougdliu@gmail.com>
2020-02-11 20:53:11 -0700

    1. Added Pathweaver Information 2. Updated NavX library 3. Updated CTRE Phoenix library

--- src/main/java/frc/robot/subsystems/DriveTrain_Subsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrain_Subsystem.java
@@ -7,10 +7,11 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.FollowerType;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 
-import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -18,6 +19,7 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -28,12 +30,16 @@ public class DriveTrain_Subsystem extends SubsystemBase {
      */
 
     /* Left Motors */
-    private final TalonFX m_leftFront = new TalonFX(Constants.MOTOR_LEFTFRONT);     // Left Front Motor using PWM Victor
-    private final TalonFX m_leftRear = new TalonFX(Constants.MOTOR_LEFTREAR);       // Left Rear Motor using PWM Victor
+    private final WPI_TalonFX m_leftFront = new WPI_TalonFX(Constants.MOTOR_LEFTFRONT);     // Left Front Motor using PWM Victor
+    private final WPI_TalonFX m_leftRear = new WPI_TalonFX(Constants.MOTOR_LEFTREAR);       // Left Rear Motor using PWM Victor
+    private final SpeedControllerGroup m_Left = new SpeedControllerGroup(m_leftFront, m_leftRear);
 
     /* Right Motors */
-    private final TalonFX m_rightFront = new TalonFX(Constants.MOTOR_RIGHTFRONT);   // Right Front Motor using PWM Victor
-    private final TalonFX m_rightRear = new TalonFX(Constants.MOTOR_RIGHTREAR);     // Right Rear Motor using PWM Victor
+    private final WPI_TalonFX m_rightFront = new WPI_TalonFX(Constants.MOTOR_RIGHTFRONT);   // Right Front Motor using PWM Victor
+    private final WPI_TalonFX m_rightRear = new WPI_TalonFX(Constants.MOTOR_RIGHTREAR);     // Right Rear Motor using PWM Victor
+    private final SpeedControllerGroup m_Right = new SpeedControllerGroup(m_rightFront, m_rightRear);
+
+    private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_Left, m_Right);
 
     AHRS gyro = new AHRS(SPI.Port.kMXP);
 
@@ -42,18 +48,36 @@ public class DriveTrain_Subsystem extends SubsystemBase {
 
     public DriveTrain_Subsystem() {
         // DriveTrain Subsystem Constructor //
+        
+        // Follow the motors //
+        // m_leftRear.follow(m_leftFront, FollowerType.PercentOutput);
+        // m_rightRear.follow(m_rightFront, FollowerType.PercentOutput);
+        
+        // Invert the left from the right motor //
+        // m_rightFront.setInverted(true);
+        // m_leftFront.setInverted(false);
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        getHeading();
     }
 
     public Rotation2d getHeading(){
         return Rotation2d.fromDegrees(-gyro.getAngle());
     }
 
-/*     public void arcadeDrive(double drive_speed, double steer_speed){
+    public DifferentialDriveWheelSpeeds getSpeeds(){
+        double gearRatio = 10.71;
+        double wheelCircumference = 2 * Math.PI * Units.inchesToMeters(3.0);
+        return new DifferentialDriveWheelSpeeds(
+            m_leftFront.getSensorCollection().getIntegratedSensorVelocity() / gearRatio * wheelCircumference / 60,
+            m_rightFront.getSensorCollection().getIntegratedSensorVelocity() / gearRatio * wheelCircumference / 60
+        );
+    }
+
+    public void arcadeDrive(double drive_speed, double steer_speed){
         // Read joystick input and translate into arcade drive //
         m_robotDrive.arcadeDrive(drive_speed, steer_speed);
     }
@@ -61,5 +85,5 @@ public class DriveTrain_Subsystem extends SubsystemBase {
     public void tankDrive(double left_speed, double right_speed){
         // Read joystick input and translate into tank drive //
         m_robotDrive.tankDrive(left_speed, right_speed);
-    } */
+    }
 }
